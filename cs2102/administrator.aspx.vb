718ed7d3058f6e5a18309a19887ec9aa8e1994dd
spvn <wkan2003@gmail.com>
2013-11-15 18:00:54 +0800

    Admin Gridview Edit checks dates

--- cs2102/administrator.aspx.vb
+++ cs2102/administrator.aspx.vb
@@ -11,6 +11,7 @@ Partial Class administrator
     Dim roomTable As New DataTable()
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
         Dim connStr As String = "Database=akaspvnc_cs2102;Data Source=sql.byethost22.org;User Id=akaspvnc_cs2102;Password=oohjingrocks;"
         Dim sqlconn As New MySqlConnection(connStr)
         Dim copyHotelNames As String
@@ -32,6 +33,46 @@ Partial Class administrator
 
         sqlconn.Close()
 
+    End Sub
+
+    Protected Sub GridView1_RowUpdating(ByVal sender As Object, ByVal e As GridViewUpdateEventArgs) Handles GridView1.RowUpdating
+        Dim row = GridView1.Rows(e.RowIndex)
+        Dim startDate = (CType((row.Cells(7).Controls(0)), TextBox)).Text
+        Dim endDate = (CType((row.Cells(8).Controls(0)), TextBox)).Text
+        Dim hotel = (CType((row.Cells(2).Controls(0)), TextBox)).Text
+        Dim room = (CType((row.Cells(4).Controls(0)), TextBox)).Text
+
+        Dim connStr As String = "Database=akaspvnc_cs2102;Data Source=sql.byethost22.org;User Id=akaspvnc_cs2102;Password=oohjingrocks;"
+        Dim sqlconn As New MySqlConnection(connStr)
+
+        Dim checkDatesStr = "SELECT start_date, end_date FROM Booking WHERE hotel_ID = @hotel_ID and room = @room"
+
+        Dim checkDateCmd As New MySqlCommand(checkDatesStr, sqlconn)
+        sqlconn.Open()
+
+        checkDateCmd.Parameters.AddWithValue("@hotel_ID", hotel)
+        checkDateCmd.Parameters.AddWithValue("@room", room)
+
+        Dim dateCheckAdapter As New MySqlDataAdapter()
+        dateCheckAdapter.SelectCommand = checkDateCmd
+
+        Dim dateTable As New DataTable
+
+        dateCheckAdapter.Fill(dateTable)
+
+        For Each drow As DataRow In dateTable.Rows
+            If (startDate >= CType(drow(0), Date) And startDate <= CType(drow(1), Date)) Or
+                (endDate >= CType(drow(0), Date) And endDate <= CType(drow(1), Date)) Or
+                (startDate <= CType(drow(0), Date) And endDate >= CType(drow(1), Date)) Then
+                errorLabel.Text = "Room is not available for the time period specified!"
+                errorLabel.Visible = True
+                e.Cancel = True
+                Return
+            End If
+        Next
+
+
+        sqlconn.Close()
     End Sub
 
     Protected Sub hotel_IDinput_TextChanged() Handles hotel_IDinput.TextChanged
